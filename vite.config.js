dce1b5943c41b696acd5e0e1f46c79d0dd8e448d
MasterZeeno <zeenoliev@gmail.com>
2024-09-15 19:00:05 +0800

    2024-09-15T18:52:26 +0800
    Modified files:
    
    - .gitignore
    package.json
    vite.config.js
    Deleted files:
    
    - dist/AlZc_zVDs5XpmO7yn3w7flUoytXJp3z29uEwmEMLEJljLXvT8UJSZQB2C1Y.woff2
    dist/AlZc_zVDs5XpmO7yn3w7flUoytXJp3z29uEwmEMLEJljLXvT8UJSZQB4C1b8GA.woff2
    dist/AlZc_zVDs5XpmO7yn3w7flUoytXJp3z29uEwmEMLEJljLXvT8UJSZQB5C1b8GA.woff2
    dist/webfonts.css
    New files:
    
    -

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
-import webfontDownload from 'vite-plugin-webfont-dl'
 import camelCase from 'camelcase'
 import packageJson from './package.json'
 
@@ -28,10 +27,5 @@ export default defineConfig({
       '@': srcFolder,
       '+': assetsFolder
     }
-  },
-  plugins: [
-    webfontDownload([
-      "https://fonts.googleapis.com/css2?family=Tilt+Warp&display=swap"
-    ])
-  ]
+  }
 })

5824a98f237e529ebed5d90c4d444ca2cc4afd6b
MasterZeeno <zeenoliev@gmail.com>
2024-09-15 10:17:00 +0800

    Sun, Sep 15, 2024 10:17:00
    Modified files:
    
    - dist/tamsv3.user.js
    - src/assets/style.css
    - src/scss/helpers/_zee-reboot.scss
    - src/tamsv3.js
    
    Changes in dist/tamsv3.user.js:
    
    diff --git a/dist/tamsv3.user.js b/dist/tamsv3.user.js
    index f0a2c4f..83fdb32 100644
    --- a/dist/tamsv3.user.js
    +++ b/dist/tamsv3.user.js
    @@ -8,4 +8,4 @@
     // @run-at     ...
    
    Changes in src/assets/style.css:
    
    diff --git a/src/assets/style.css b/src/assets/style.css
    index b94c9b7..5b431bb 100644
    --- a/src/assets/style.css
    +++ b/src/assets/style.css
    @@ -1 +1 @@
    -﻿@font-face{f...
    
    Changes in src/scss/helpers/_zee-reboot.scss:
    
    diff --git a/src/scss/helpers/_zee-reboot.scss b/src/scss/helpers/_zee-reboot.scss
    index 76ecbf6..4bbf82c 100644
    --- a/src/scss/helpers/_zee-reboot.scss
    +++ b/src/scss/h...
    
    Changes in src/tamsv3.js:
    
    diff --git a/src/tamsv3.js b/src/tamsv3.js
    index b16d3bf..c091462 100644
    --- a/src/tamsv3.js
    +++ b/src/tamsv3.js
    @@ -1,31 +1,50 @@
     import logo from './assets/logo.svg?r...

--- src/tamsv3.js
+++ src/tamsv3.js
@@ -1,30 +1,50 @@
 import logo from './assets/logo.svg?raw'
 import styles from './assets/style.css?raw'
 
+const title = "TamsV3"
+
 window.addEventListener('load', function() {
   if (document.body) {
-    document.body.style.display = 'none'
+    document.body.style.opacity = '0'
+    document.documentElement.setAttribute("data-zee-theme", "dark")
+    document.title = title
+
+    const logoContainer = document.querySelector(".text-center img")?.parentNode
+    if (logoContainer) {
+      logoContainer.innerHTML = logo
+    }
+
+    const loginAnchor = document.querySelector(".login-logo a")
+    if (loginAnchor) {
+      loginAnchor.innerHTML = title
+    }
+
+    const usernameInput = document.querySelector('input[name="username"]')
+    if (usernameInput) {
+      usernameInput.setAttribute("placeholder", "ID Number")
+    }
+
+    const style = document.createElement("style")
+    style.innerHTML = styles
+
     setTimeout(() => {
-      document.body.style.display = 'flex'
+      document.head.appendChild(style)
+      document.body.style.opacity = '1'
     }, 600)
   }
 }, false)
 
-const title = "TamsV3"
-
 const removeElements = (parent, selector = []) => {
   if (!(parent instanceof HTMLElement || parent instanceof Document)) return
 
-  const selectorString = Array.isArray(selector) ? selector.join(",") : selector
-
+  const selectorString = Array.isArray(selector) ? selector.join(",") : ''
+  
   parent.querySelectorAll(selectorString).forEach((el) => {
-    if (el !== parent) {
-      if (el.id !== "important") {
-        try {
-          el.remove()
-        } catch (err) {
-          console.error('Error removing element:', err)
-        }
+    if (el !== parent && el.id !== "important") {
+      try {
+        el.remove()
+      } catch (err) {
+        console.error('Error removing element:', err)
       }
     }
   })
@@ -33,27 +53,8 @@ const removeElements = (parent, selector = []) => {
 removeElements(document, ["p.login-box-msg", "span.glyphicon"])
 removeElements(document, ["link", "script", "style"])
 
-document.documentElement.setAttribute("data-zee-theme", "dark")
-document.documentElement.setAttribute("style", "background-color: white;")
-document.title = title
-
-const logoContainer = document.querySelector(".text-center img")?.parentNode
-if (logoContainer) {
-  logoContainer.innerHTML = logo
-}
-
-const loginAnchor = document.querySelector(".login-logo a")
-if (loginAnchor) {
-  loginAnchor.innerHTML = title
-}
-
-const usernameInput = document.querySelector('input[name="username"]')
-if (usernameInput) {
-  usernameInput.setAttribute("placeholder", "ID Number")
-}
-
-const style = document.createElement("style")
-style.innerHTML = styles
-document.head.appendChild(style)
-
-import("bootstrap").then(module => module())
+import("bootstrap").then(module => {
+  if (typeof module.default === "function") {
+    module.default()
+  }
+})

98d6127d7c30a72a264228fc010d0ab4dcf56517
MasterZeeno <zeenoliev@gmail.com>
2024-09-16 15:12:49 +0800

    fix: index.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import camelCase from 'camelcase'
 import packageJson from './package.json'
 
 const packageName = packageJson.name.split('/').pop() || packageJson.name
-const srcFolder = resolve(__dirname, 'src')
-const assetsFolder = resolve(srcFolder, 'assets')
 
 export default defineConfig({
   build: {
@@ -13,7 +11,7 @@ export default defineConfig({
       polyfill: true
     },
     lib: {
-      entry: resolve(srcFolder, 'index.js'),
+      entry: resolve(__dirname, 'src', 'index.js'),
       formats: ['iife'],
       name: camelCase(packageName, {
         pascalCase: true
@@ -22,10 +20,18 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 999
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        additionalData: `$prefix: zee-;`
+      }
+    },
+    preprocessorMaxWorkers: true
+  },
   resolve: {
     alias: {
-      '@': srcFolder,
-      '+': assetsFolder
+      '@': resolve(__dirname, 'src')
     }
   }
 })
